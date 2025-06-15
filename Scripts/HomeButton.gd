2a70797a5f51105c164235831c9d7e3a760b0aa0
David Dalisay <urodelagames@gmail.com>
2025-06-14 20:02:08 -0400

    Home button no longer opens new tab

--- Scripts/HomeButton.gd
+++ Scripts/HomeButton.gd
@@ -2,4 +2,8 @@ extends Button
 
 
 func _on_pressed():
-	OS.shell_open("https://playlethal.fun/")
+	#OS.shell_open("https://playlethal.fun/")
+	open_window_in_same_tab("https://playlethal.fun/")
+	
+func open_window_in_same_tab(url: String) -> void:
+	JavaScriptBridge.eval("window.location.href = '%s';" % url)
