b14a9310b5cd2bf531bfa4f626d6be25b0fcde5c
David Dalisay <urodelagames@gmail.com>
2026-03-09 08:36:17 -0400

    Target description.

--- Scripts/TargetCard.gd
+++ Scripts/TargetCard.gd
@@ -1,5 +1,5 @@
 extends Card
 
 func update_description_panel() -> void:
-	card_description = "Gain 1 [color=#e67e22]Critical[/color]."
+	card_description = "Gain 1 [color=#e67e22]Critical[/color]. Your next attack does double damage."
 	$DescriptionPanel/Title.set_text("[center][shake rate=2.0 level=1 connected=1]%s[/shake][/center]" % card_description)
